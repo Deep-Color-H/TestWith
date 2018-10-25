56c327eac5f50cbea88a01c670d01fb1fab97d1e
DESKTOP-3J7V81N\Playdata <kyuinkyuin305@gmail.com>
2018-10-25 14:47:28 +0900

    디벨롭 업데이트

--- Test/src/com/test/Hello.java
+++ Test/src/com/test/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		
 		System.out.println("Hello, World!\n");
 		System.out.println("I'm gyuin, what is your name?");
-		
+		System.out.println("Aah, your name is Dong-Ook. Nice to meet you!");
 	}
 	
 }
