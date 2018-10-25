6cc8c2e5051e1bb33544b6abe2de34c48d034c01
DESKTOP-3J7V81N\Playdata <kyuinkyuin305@gmail.com>
2018-10-25 14:46:14 +0900

    한글 버전 추가

--- Test/src/com/test/Hello.java
+++ Test/src/com/test/Hello.java
@@ -4,8 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello, World!\n");
-		System.out.println("I'm gyuin, what is your name?");
+		System.out.println("안녕, 세계야!\n");
+		System.out.println("나는 규인이야, 니 이름은 뭐니?");
 		
 	}
 	
