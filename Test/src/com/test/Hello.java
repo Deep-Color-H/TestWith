ada5c519be7d616ce69df0734fbf6f17738e3b76
DESKTOP-3J7V81N\Playdata <kyuinkyuin305@gmail.com>
2018-10-25 14:58:13 +0900

    Spanish

--- Test/src/com/test/Hello.java
+++ Test/src/com/test/Hello.java
@@ -4,9 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello, World!\n");
-		System.out.println("I'm gyuin, what is your name?");
-		System.out.println("Aah, your name is Dong-Ook. Nice to meet you!");
+		System.out.println("Hola! Que Tal?\n");
 	}
 	
 }
